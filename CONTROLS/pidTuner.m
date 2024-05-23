7a097f189dda790fc3de0680e2c366d181a03061
Amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-05-23 19:04:24 +0300

    Open loop

--- CONTROLS/pidTuner.m
+++ CONTROLS/pidTuner.m
@@ -1,1 +1,16 @@
+%defining open loop TF
+numerator=1;
+denominator = [1,10,20];
+s = tf('s');
+P= tf(numerator, denominator);
 
+%Generating a P controller pidtool (P, 'p')
+pidtool(P, 'p')
+
+%Generating a PID controller 
+C = pid(500,400,50);
+pidtool(P, C);
+
+%Using pidtune 
+[autoPID, info] = pidtune(P, C); 
+closedsys_autopid = feedback (autoPID*P, 1);
